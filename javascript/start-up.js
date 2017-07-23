c732c805aae0097d8023af74fc7f5864cbda5722
Ica Riluci <weiyh15@mails.tsinghua.edu.cn>
2017-07-23 09:24:39 +0800

    update animation in main menu

---
+++ javascript/start-up.js
@@ -0,0 +1,17 @@
+function startgame() {
+    $('#start-up h1').addClass('off');
+    $('#start-up button').addClass('off');
+}
+function showpreference() {
+    $('#start-up h1').addClass('off');
+    $('#start-up button').addClass('off');
+}
+function showcareer() {
+    $('#start-up h1').addClass('off');
+    $('#start-up button').addClass('off');
+}
+function showhelp() {
+    $('#start-up h1').addClass('off');
+    $('#start-up button').addClass('off');
+
+}
