3b4ab259c71ef33da605057c260dc87eb7f59168
Ica Riluci <weiyh15@mails.tsinghua.edu.cn>
2017-07-24 18:20:21 +0800

    animation and ui update

--- javascript/pop-out.js
+++ javascript/pop-out.js
@@ -1,14 +1,23 @@
 function hidepreference() {
     $('#preferencewindow .menubar').addClass('hide');
     $('#preferencewindow').addClass('off');
+    $('#heroselect').rmClass('off');
+    $('#start-up h1').rmClass('off');
+    $('#start-up button.button').rmClass('off');
 }
 
 function hidecareer() {
     $('#careerwindow .menubar').addClass('hide');
     $('#careerwindow').addClass('off');
+    $('#heroselect').rmClass('off');
+    $('#start-up h1').rmClass('off');
+    $('#start-up button.button').rmClass('off');
 }
 
 function hidehelp() {
     $('#helpwindow .menubar').addClass('hide');
     $('#helpwindow').addClass('off');
+    $('#heroselect').rmClass('off');
+    $('#start-up h1').rmClass('off');
+    $('#start-up button.button').rmClass('off');
 }
--- javascript/start-up.js
+++ javascript/start-up.js
@@ -1,19 +1,29 @@
 function startgame() {
     $('#start-up h1').addClass('off');
-    $('#start-up button').addClass('off');
+    $('#start-up button.button').addClass('off');
+    $('#heroselect').addClass('off');
 }
 function showpreference() {
     $('#preferencewindow').rmClass('off');
     $('#preferencewindow .menubar').rmClass('off');
     $('#preferencewindow .menubar').rmClass('hide');
+    $('#start-up h1').addClass('off');
+    $('#start-up button.button').addClass('off');
+    $('#heroselect').addClass('off')
 }
 function showcareer() {
     $('#careerwindow').rmClass('off');
     $('#careerwindow .menubar').rmClass('off');
     $('#careerwindow .menubar').rmClass('hide');
+    $('#start-up h1').addClass('off');
+    $('#start-up button.button').addClass('off');
+    $('#heroselect').addClass('off')
 }
 function showhelp() {
     $('#helpwindow').rmClass('off');
     $('#helpwindow .menubar').rmClass('off');
     $('#helpwindow .menubar').rmClass('hide');
+    $('#start-up h1').addClass('off');
+    $('#start-up button.button').addClass('off');
+    $('#heroselect').addClass('off')
 }
